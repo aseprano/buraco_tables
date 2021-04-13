e8fbc791097e95a6d2532292aab5dc4649e996d6
Antonio Seprano <seprano.antonio@gmail.com>
2021-04-13 23:45:47 +0200

    Added TableReadyForPlaying event

---
+++ src/domain/events/TableReadyForPlaying.ts
@@ -0,0 +1,23 @@
+import { DomainEvent } from './DomainEvent';
+import { TableID } from '../value_objects/TableID';
+
+const EventName = 'com.herrdoktor.buraco.events.TableReadyForPlaying';
+
+export class TableReadyForPlaying extends DomainEvent {
+    public static readonly EventName = EventName;
+
+    public constructor(
+        table: TableID,
+    ) {
+        super();
+
+        this.setPayload({
+            table: table.asNumber(),
+        });
+    }
+
+    public getName(): string {
+        return EventName;
+    }
+
+}
---
+++ tests/events/TableReadyForPlaying.spec.ts
@@ -0,0 +1,16 @@
+import { TableReadyForPlaying } from '../../src/domain/events/TableReadyForPlaying';
+import { TableID } from '../../src/domain/value_objects/TableID';
+
+describe('TableReadyForPlaying', () => {
+
+    it('has the proper data', () => {
+        expect(TableReadyForPlaying.EventName).toEqual('com.herrdoktor.buraco.events.TableReadyForPlaying');
+
+        const event = new TableReadyForPlaying(new TableID(123));
+        expect(event.getName()).toEqual(TableReadyForPlaying.EventName);
+        expect(event.getPayload()).toEqual({
+            table: 123,
+        });
+    });
+
+});
